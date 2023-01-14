f1b4c99ab0284c998731e43572c93ae98e16ed6c
Merwil Varona <varona.merwil@auf.edu.ph>
2023-01-14 23:36:48 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const container = document.querySelector(".container"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+    //   hide password
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "text";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    })
+                }else{
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    })
+                }
+            }) 
+        })
+    })
+
+    // appear signup and login form
+    signUp.addEventListener("click", ( )=>{
+        container.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container.classList.remove("active");
+    });
+
+    // login authentication
+    function auth(){
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+        if(email== "user@gmail.com" && password== "password123"){
+            window.location.assign("dashboard.html");
+            alert("Login Successfully!")
+        }
+        else{
+            alert("Incorrect email or password");
+            return;
+        }
+    }
+
+
+    
